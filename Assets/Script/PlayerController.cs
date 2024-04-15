7fe2a13afc46401a58a93f40a6872ef20a3e1b67
aak-ritie <aakritibhusal234@gmail.com>
2024-04-15 14:27:23 +0545

    Complete

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     public  Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
     void Start()
     {
-        
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }   
     private void FixedUpdate()
     {
@@ -17,4 +22,22 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         rb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) *speed);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+    public void SetCountText()
+    {
+        countText.text = count.ToString();
+        if(count >= 9)
+        {
+            winText.text = "You Win!";
+        }
+    }
+    
 }
